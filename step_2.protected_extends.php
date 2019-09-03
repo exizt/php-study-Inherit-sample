c1e1f66f2a8578688211ee632ed721a709379a9a
exizt <e2xist@gmail.com>
2019-09-04 01:21:01 +0900

    Create step_2.protected_extends.php

---
+++ step_2.protected_extends.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * protected member variable with extends
+ */
+class A  extends B{
+  protected $name;
+  public function setName($name){
+    $this->name = $name;
+  }
+  public function printName(){
+    echo "A.name [".$this->name. "]\n<br>";
+  }
+}
+class B{
+  protected $name;
+  public function setName($name){
+    $this->name = $name;
+  }
+  public function printName(){
+    echo "B.name [".$this->name. "]\n<br>";
+  }
+  public function printName2(){
+    echo "B.name (printName2) [".$this->name. "]\n<br>";
+  }
+  public function callPrintName(){
+    $this->printName();
+  }
+}
+$a = new A;
+$a->setName('ddd');
+$a->printName(); // A.name [ddd] 
+$a->printName2(); // B.name (printName2) [ddd] 
+$a->callPrintName(); // A.name [ddd] 
